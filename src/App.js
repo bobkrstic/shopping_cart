24e4e8cd9d577e06325f2a4a302cd2b94aa513b0
bobkrstic <slobodankrsticnis@yahoo.com>
2019-11-02 16:37:50 -0700

    Working with main components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import { Container } from "react-bootstrap";
-// import Bootstrap from "react-bootstrap";
-// import "bootstrap/dist/css/bootstrap.min.css";
+import Subtotal from "./components/Subtotal/Subtotal";
+import PickupSavings from "./components/PickupSavings/PickupSavings";
+import TaxesFees from "./components/TaxesFees/TaxesFees";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="container">
-      <Container className="purchase-card">
-        <h1>Hello World</h1>
-      </Container>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: 100,
+      PickupSavings: -3.85,
+      taxes: 0
+    };
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Container className="purchase-card">
+          <Subtotal price={this.state.total.toFixed(2)} />
+          <PickupSavings price={this.state.PickupSavings} />
+          <TaxesFees taxes={this.state.taxes.toFixed(2)} />
+          <hr />
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
